fc4b6cdd807ce0e3e959efeba91e228510016d31
Muhammad Abdullah <100953810+muhammadabdullah15@users.noreply.github.com>
2022-10-11 21:24:13 +0500

    Add files via upload

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -14,16 +14,63 @@ private:
 
     void destroySubTree(treeNode *nodePtr)
     {
-        return;
+        if (nodePtr == NULL)
+            return;
+
+        /* first delete both subtrees */
+        destroySubTree(nodePtr->left);
+        destroySubTree(nodePtr->right);
+
+        /* then delete the node */
+        cout << "\n Deleting node: " << nodePtr->value;
+        delete nodePtr;
     }
 
     void deleteNode(int value, treeNode *&nodePtr)
     {
-        return;
+        if (value < nodePtr->value)
+            deleteNode(value, nodePtr->left);
+        else if (value > nodePtr->value)
+            deleteNode(value, nodePtr->right);
+        else
+            makeDeletion(nodePtr);
     }
 
     void makeDeletion(treeNode *&nodePtr)
     {
+        treeNode *tempPtr; // Used for reattaching left subtree
+
+        if (!nodePtr)
+        {
+            cout << "Cannot delete empty node\n";
+            return;
+        }
+
+        if (!nodePtr->right)
+        {
+            tempPtr = nodePtr;
+            nodePtr = nodePtr->left; // Reattach left child
+            delete tempPtr;
+            return;
+        }
+
+        if (!nodePtr->left)
+        {
+            tempPtr = nodePtr;
+            nodePtr = nodePtr->right; // Reattach right child
+            delete tempPtr;
+            return;
+        }
+
+        tempPtr = tempPtr->right;
+        while (tempPtr->left)
+            tempPtr = tempPtr->left;
+
+        tempPtr->left = nodePtr->left; // Left subtree reattached
+
+        tempPtr = nodePtr;
+        nodePtr = nodePtr->right; // Right subtree reattached
+        delete tempPtr;
         return;
     }
 
@@ -61,6 +108,22 @@ private:
         }
 
         return;
+    }
+
+    treeNode *lowestCommonAncestor(treeNode *root, int n1, int n2)
+    {
+        if (root == NULL)
+            return NULL;
+
+        // If both n1 and n2 are smaller than root, then LCA lies in left
+        if (root->value > n1 && root->value > n2)
+            return lowestCommonAncestor(root->left, n1, n2);
+
+        // If both n1 and n2 are greater than root, then LCA lies in right
+        if (root->value < n1 && root->value < n2)
+            return lowestCommonAncestor(root->right, n1, n2);
+
+        return root;
     }
 
 public:
@@ -113,7 +176,7 @@ public:
             }
             else
             {
-                cout << "03074344309 Lun na khara hone ki surat mein iss number pe rabta karein ~ Umer Tayyab";
+                cout << "Value already in tree" << endl;
                 return;
             }
         }
@@ -154,10 +217,49 @@ public:
         return false;
     }
 
-    
+    void remove(int num)
+    {
+        deleteNode(num, root);
+        return;
+    }
+
+    int getLCA(int n1, int n2)
+    {
+        return (lowestCommonAncestor(root, n1, n2)->value);
+    }
+
+    int minValue(treeNode *node)
+    {
+        treeNode *temp = node;
+
+        /* loop down to find the leftmost leaf */
+        while (temp->left != NULL)
+            temp = temp->left;
+
+        return (temp->value);
+    }
+
+    int maxValue(treeNode *node)
+    {
+        treeNode *temp = node;
+
+        /* loop down to find the leftmost leaf */
+        while (temp->right != NULL)
+            temp = temp->right;
+
+        return (temp->value);
+    }
 };
 
 int main()
 {
+    BinaryTree tree;
+    cout << "Inserting nodes. ";
+    tree.insertNode(5);
+    tree.insertNode(8);
+    tree.insertNode(3);
+    tree.insertNode(12);
+    tree.insertNode(9);
+    tree.printNodesInOrder();
     return 0;
 }
