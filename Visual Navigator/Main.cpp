560f3f286a3234d74b234101bb5fc8740099539d
Muhammad Abdullah <100953810+muhammadabdullah15@users.noreply.github.com>
2022-12-18 18:15:32 +0500

    Add files via upload

--- Visual Navigator/Main.cpp
+++ Visual Navigator/Main.cpp
@@ -9,6 +9,9 @@
 using namespace std;
 // g++ -c pr.cpp;g++ pr.o -o sfml-app -lsfml-graphics -lsfml-window -lsfml-system;./sfml-app
 
+#define NUM_LOCATIONS 50
+#define NUM_JUNCTIONS 100
+
 struct Location
 {
     string name;
@@ -22,37 +25,39 @@ ostream &operator<<(ostream &os, const Location &l)
     return os;
 }
 
-int distanceBetweenLocations(Location l1, Location l2)
+int distanceBetweenPoints(Location l1, Location l2)
 {
-    return int(pow(((pow((l2.cordX - l1.cordX), 2)) - (pow((l2.cordY - l1.cordY), 2))), 0.5));
+    // cout << l1.cordX << "," << l1.cordY << endl
+    //      << l2.cordX << "," << l2.cordY << endl;
+    return sqrt(pow(l1.cordX - l2.cordX, 2) + pow((l1.cordY - l2.cordY), 2));
+    // return sqrt((pow((l2.cordX - l1.cordX), 2)) - (pow((l2.cordY - l1.cordY), 2)));
 }
 
-void initializeVisibleNodes(Location visibleNodes[50])
+void initializeVisibleNodes(Location visibleNodes[NUM_LOCATIONS])
 {
     ifstream readfile;
-    readfile.open("coordinates.txt");
-    for (int i = 0; !readfile.eof() && i < 50; i++)
+    readfile.open("coords_locations.txt");
+    for (int i = 0; !readfile.eof() && i < NUM_LOCATIONS; i++)
     {
         readfile >> visibleNodes[i].cordX;
         readfile >> visibleNodes[i].cordY;
         readfile >> visibleNodes[i].name;
-        cout << "name len" << i << " : " << (visibleNodes[i].name).length() << endl;
+        replace(visibleNodes[i].name.begin(), visibleNodes[i].name.end(), '-', ' ');
+        // cout << "name len" << i << " : " << (visibleNodes[i].name).length() << endl;
         if (visibleNodes[i].name == "")
             break;
     }
 
-    for (int i = 0; i < 50 && visibleNodes[i].name != ""; i++)
-    {
-        cout << "ID: " << i << endl
-             << visibleNodes[i];
-    }
+    // for (int i = 0; i < NUM_LOCATIONS && visibleNodes[i].name != ""; i++)
+    // {
+    //     cout << "ID: " << i << endl
+    //          << visibleNodes[i];
+    // }
     readfile.close();
 }
 
-void displayMarkers(sf::RenderWindow &window, Location visibleNodes[50])
+void displayMarkers(sf::RenderWindow &window, Location visibleNodes[NUM_LOCATIONS])
 {
-    Location temp;
-
     sf::CircleShape marker(10.f);
     sf::Color clr(0xb33f0dff);
     marker.setFillColor(clr);
@@ -60,34 +65,50 @@ void displayMarkers(sf::RenderWindow &window, Location visibleNodes[50])
     // marker.setPosition(-10, -10);
     // window.draw(marker);
 
-    for (int i = 0; i < 50; i++)
+    for (int i = 0; i < NUM_LOCATIONS && visibleNodes[i].name != ""; i++)
     {
-        if (visibleNodes[i].name == "")
-            break;
-        // cout << "Drawing" << temp << endl;
-        marker.setPosition(temp.cordX - 10, temp.cordY - 10);
+        // if (visibleNodes[i].name == "")
+        //     break;
+        // // cout << "Drawing" << temp << endl;
+        marker.setPosition(visibleNodes[i].cordX - 10, visibleNodes[i].cordY - 10);
         window.draw(marker);
     }
     return;
 }
 
-int click(int cordX, int cordY, Location visibleNodes[50])
+void addPoint(int i, Location visibleNodes[NUM_LOCATIONS])
 {
+    cout << visibleNodes[i] << endl;
+}
+
+void click(int cordX, int cordY, Location visibleNodes[NUM_LOCATIONS])
+{
+    Location temp;
+    temp.cordX = cordX;
+    temp.cordY = cordY;
+
+    for (int i = 0; i < NUM_LOCATIONS && visibleNodes[i].name != ""; i++)
+    {
+        // cout << "id:" << i << ": " << visibleNodes[i].name << "-" << distanceBetweenPoints(visibleNodes[i], temp) << endl;
+        if (distanceBetweenPoints(visibleNodes[i], temp) >= 0 && distanceBetweenPoints(visibleNodes[i], temp) <= 10)
+        {
+            addPoint(i, visibleNodes);
+            return;
+        }
+    }
+
+    return;
 }
 
 int main()
 {
-    Location visibleNodes[50];
+    Location visibleNodes[NUM_LOCATIONS];
+    Location roadJunctions[NUM_JUNCTIONS];
 
     initializeVisibleNodes(visibleNodes);
     // test(visibleNodes);
 
     sf::RenderWindow window(sf::VideoMode(869, 613), "GIKI Navigator");
-
-    // sf::RectangleShape rectangle(sf::Vector2f(100.0f, 300.0f));
-    // rectangle.setFillColor(sf::Color::Red);
-    // rectangle.setPosition(320, 0);
-    // rectangle.setPosition(320, 400);
 
     sf::Texture backGround;
     sf::Sprite sBg;
@@ -95,6 +116,11 @@ int main()
         std::cout << "File not found\n";
 
     sBg.setTexture(backGround);
+
+    sf::VertexArray lines(sf::LinesStrip, 2);
+    lines[0].color = lines[1].color = sf::Color::Red;
+    lines[0].position = sf::Vector2f(0, 0);
+    lines[1].position = sf::Vector2f(100, 100);
 
     while (window.isOpen())
     {
@@ -105,12 +131,16 @@ int main()
                 window.close();
 
             if (event.type == sf::Event::MouseButtonReleased)
-                std::cout << sf::Mouse::getPosition(window).x << "," << sf::Mouse::getPosition(window).y << std::endl;
+            {
+                cout << "Click Location: " << sf::Mouse::getPosition(window).x << "," << sf::Mouse::getPosition(window).y << std::endl;
+                // click(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y, visibleNodes);
+            }
         }
 
         window.draw(sBg);
+        window.draw(lines);
 
-        // displayMarkers(window, visibleNodes);
+        displayMarkers(window, visibleNodes);
         window.display();
     }
 
